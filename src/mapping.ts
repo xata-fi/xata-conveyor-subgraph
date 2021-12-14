76ec0151fa6d291d1aa77d0f2978b3a8a2146be6
r2d2 <95279505+r2d2-rmbl@users.noreply.github.com>
2021-12-14 17:57:36 +0800

    Initial commit - unable to decode metaTxns

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,68 +1,70 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import {Address, BigDecimal, BigInt, ethereum, log} from "@graphprotocol/graph-ts"
 import {
   ConveyorV2Router01,
   MetaStatus,
   OwnershipTransferred
 } from "../generated/ConveyorV2Router01/ConveyorV2Router01"
-import { ExampleEntity } from "../generated/schema"
+import { MetaTransaction, User } from "../generated/schema"
+import {Transfer} from "../generated/templates/ERC20/ERC20";
 
-export function handleMetaStatus(event: MetaStatus): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.sender = event.params.sender
-  entity.success = event.params.success
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.DOMAIN_TYPEHASH(...)
-  // - contract.FORWARDER_TYPEHASH(...)
-  // - contract.addLiquidity(...)
-  // - contract.executeMetaTx(...)
-  // - contract.factory(...)
-  // - contract.feeHolder(...)
-  // - contract.getAmountIn(...)
-  // - contract.getAmountOut(...)
-  // - contract.getAmountsIn(...)
-  // - contract.getAmountsOut(...)
-  // - contract.metaEnabled(...)
-  // - contract.nonces(...)
-  // - contract.owner(...)
-  // - contract.quote(...)
-  // - contract.relayers(...)
-  // - contract.removeLiquidityWithPermit(...)
-  // - contract.swapExactTokensForTokens(...)
-  // - contract.swapTokensForExactTokens(...)
+// Compares if 2 Address or String types refer to the same address.
+function addressEquals(add1: string, add2: string): boolean {
+  return add1.toLowerCase() == add2.toLowerCase();
 }
 
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+function getMetaTransaction(txnId: string): MetaTransaction {
+  let metaTxn = MetaTransaction.load(txnId)
+  if (!metaTxn) {
+    metaTxn = new MetaTransaction(txnId)
+
+    metaTxn.gasToken = Address.zero();
+    metaTxn.gasTokenDecimal = 0;
+    metaTxn.gasAmount = BigDecimal.zero();
+    metaTxn.unusedSlippageUSD = BigDecimal.zero();
+    metaTxn.sender = '';
+    metaTxn.success = false;
+  }
+  return metaTxn;
+}
+
+export function handleMetaStatus(event: MetaStatus): void {
+  let metaTxn = getMetaTransaction(event.transaction.hash.toHexString());
+
+  //Set sender and meta status info
+  const originalSenderAddress = event.params.sender;
+  let sender = User.load(originalSenderAddress.toHexString());
+  if (!sender) {
+    sender = new User(originalSenderAddress.toHexString());
+    sender.gasPaidUSD = BigDecimal.zero();
+  }
+  metaTxn.sender = sender.id;
+  metaTxn.success = event.params.success;
+
+  //Set decoded information within meta txn
+  const metaTxnTypes = '(tuple(address,address,uint256,uint256,uint256,bytes,bytes32),string,uint256,uint256,tuple(uint8,bytes32,bytes32))';
+  let decoded = ethereum.decode(metaTxnTypes, event.transaction.input);
+
+  if (decoded) {
+    const decodedTuple = decoded.toTuple();
+    log.debug('Decoded address: {}', [decodedTuple[0].toTuple()[0].toAddress().toHexString()]);
+    log.debug('Decoded domain: {}', [decodedTuple[1].toString()]);
+  } else {
+    log.debug('Unable to decode.', []);
+  }
+
+  sender.save();
+  metaTxn.save();
+}
+
+export function handleTransfer(event: Transfer): void {
+  if (addressEquals(event.params.to.toString(), event.transaction.from.toString())) { // if transfer is sent to relayer's address
+    let metaTxn = getMetaTransaction(event.transaction.hash.toHexString());
+    metaTxn.gasToken = event.address;
+    metaTxn.gasAmount = event.params.value.toBigDecimal();
+    metaTxn.gasTokenDecimal = 18;
+    metaTxn.save();
+    log.debug('Saved gas info. for {}', [metaTxn.id]);
+  } else {
+    log.debug('{} is not a gas fee payment.', [event.transaction.hash.toHexString()]);
+  }
+}
